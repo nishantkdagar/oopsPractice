3bed044db4ecea156c71889a1bf717ba07338502
Nishan Kumar <nishantkumarkd@gmail.com>
2022-07-18 08:56:45 +0530

    Playing with Cards

--- src/com/oops/DeckOfCards.java
+++ src/com/oops/DeckOfCards.java
@@ -2,6 +2,29 @@ package com.oops;
 
 public class DeckOfCards {
     public static void main(String[] args) {
-        System.out.println("Welcome to Object Oriented Programming");
+        String[] SUITS = {"Clubs", "Diamonds", "Hearts", "Spades"};
+        String[] RANKS = {"2", "3", "4", "5", "6", "7", "8", "9", "10","Jack", "Queen", "King", "Ace"};
+
+        int n = SUITS.length * RANKS.length;
+        String[] deck = new String[n];
+        for (int i = 0; i < RANKS.length; i++) {
+            for (int j = 0; j < SUITS.length; j++) {
+                deck[SUITS.length*i + j] = RANKS[i] + " of " + SUITS[j];
+            }
+        }
+
+        for (int i = 0; i < n; i++) {
+            int r = i + (int) (Math.random() * (n-i));
+            String temp = deck[r];
+            deck[r] = deck[i];
+            deck[i] = temp;
+        }
+
+        for (int i=0;i<4;i++){
+            for (int j= 0; j < 9; j++)
+            {
+                System.out.println("Player "+(i+1)+" having cards: "+deck[j]);
+            }
+        }
     }
 }
